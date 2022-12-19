146d7faffc2a78ceb26de7b9e888d380bcdd7e79
ThereIsNoOne <szymon.s.lasota@gmail.com>
2022-12-19 21:52:22 +0100

    Export .tex, Buttons added, Pictures now working

--- util.py
+++
@@ -1,9 +0,0 @@
-from settings import WIDTH, HEIGHT
-
-
-def height_prc(percent: int) -> int:
-    return HEIGHT/100 * percent
-
-
-def width_prc(percent: int) -> int:
-    return WIDTH/100 * percent
